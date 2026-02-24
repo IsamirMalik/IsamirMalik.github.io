450284228c035b02e71bcb94ebea0dfd6b4afc21
Sameer Malik <153701541+IsamirMalik@users.noreply.github.com>
2026-02-24 23:10:04 +0530

    Update Navbar.jsx

--- new/src/Components/Navbar.jsx
+++ new/src/Components/Navbar.jsx
@@ -28,7 +28,7 @@ export default function Navbar() {
           </li>
           <li className="nav-item">
             <a
-              href="https://drive.google.com/file/d/11nVGheK7Pj1ZPa_KAz4lffKOZ5d647Nw/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1rLXIhLOP8zDVbgiDpnrlMYDVhgMs7oaM/view"
               download
               target="_blank"
             >
